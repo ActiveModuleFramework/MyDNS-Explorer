5c8509df690fa221895991e2af508410c807829e
SoraKumo <info@croud.jp>
2019-05-18 12:14:24 +0900

    サーバ用に設定変更

--- src/app/index.ts
+++ src/app/index.ts
@@ -58,15 +58,15 @@ const manager = new amf.Manager({
 	remotePath: '/',										//一般コンテンツのリモートパス
 	execPath: '/',											//コマンド実行用リモートパス
 	indexPath: path.resolve(__dirname, '../template/index.html'),//index.thmlテンプレート
-	rootPath: path.resolve(__dirname, '../public'),				//一般コンテンツのローカルパス
+	rootPath: path.resolve(__dirname, '../public'),			//一般コンテンツのローカルパス
 	cssPath: ['css'],										//自動ロード用CSSパス
 	jsPath: ['js'],											//一般コンテンツのローカルパス
-	//	localDBPath: path.resolve(__dirname,'../db/app.db'),	//ローカルDBパス
-	localDBPath: path.resolve('app.db'),	//ローカルDBパス(カレントパスに設定)
+	localDBPath: path.resolve(__dirname,'../db/app.db'),	//ローカルDBパス
+	//localDBPath: path.resolve('app.db'),	//ローカルDBパス(カレントパスに設定)
 	modulePath: path.resolve(__dirname, './modules'),		//モジュール配置パス
 	jsPriority: [],											//優先JSファイル設定
 	debug: false,											//デバッグ用メッセージ出力
-	listened,								//初期化完了後コールバック
-	listen: listenPort											//受付ポート/UNIXドメインソケット
-	//listen:'dist/sock/app.sock'							//UNIXドメインソケットを使用する場合
+	listened,												//初期化完了後コールバック
+	//listen: listenPort									//受付ポート/UNIXドメインソケット
+	listen: path.resolve(__dirname, '../sock/app.sock')		//UNIXドメインソケットを使用する場合
 })
